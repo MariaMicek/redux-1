09db822aa3273e1edb6ac9be94d1fad2ce51acc9
Maria Micek <mmicek16@wp.pl>
2019-04-15 11:49:15 +0200

    actions' type names signed to variables

--- src/state/counter.js
+++ src/state/counter.js
@@ -2,23 +2,26 @@ const initialState = {
     number: 0,
 }
 
+const INC = 'counter/INC'
+const DEC = 'counter/DEC'
+
 export const incActionCreator = () => ({
-    type: 'INC'
+    type: INC
 })
 
 export const decActionCreator = () => ({
-    type: 'DEC'
+    type: DEC
 })
 
 export const reducer = (state = initialState, action) => {
     console.log(state, action)
-    if (action.type === 'INC') {
+    if (action.type === INC) {
         return {
             ...state,
             number: state.number + 1
         }
     }
-    if (action.type === 'DEC') {
+    if (action.type === DEC) {
         return {
             ...state,
             number: state.number - 1
