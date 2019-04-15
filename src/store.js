1ab1079e1408461d754d10a19540167c71b87dec
Maria Micek <mmicek16@wp.pl>
2019-04-15 11:24:42 +0200

    DEC action and INC action

--- src/store.js
+++ src/store.js
@@ -1,11 +1,21 @@
 import { createStore } from 'redux'
 
-const reducer = (state, action) => {
-    console.log(state)
-    console.log(action)
+const initialState = {
+    number: 0,
+}
 
-    return {
-        number: 0,
+const reducer = (state = initialState, action) => {         //state = initailState - domyślny parametr, gdy nie ma innego
+    console.log(state, action)
+    if (action.type === 'INC') {
+        return {
+            ...state,
+            number: state.number + 1
+        }
+    } else if (action.type === 'DEC'){
+        return {
+            ...state,
+            number: state.number -1
+        }
     }
 }
 
@@ -13,3 +23,14 @@ export const store = createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+const incAction = {         //action to obiekt, który posiada type
+    type: 'INC'
+}
+
+const decAction = {         //action to obiekt, który posiada type
+    type: 'DEC'
+}
+
+store.dispatch(incAction)       //funkcja dispatch jest wywołana z akcją
+store.dispatch(decAction)
